de3293ae387cd28892d95a83cb232bafad16fe13
iris245ger <iris.germanica48@gmail.com>
2025-03-29 17:13:01 +0300

    Extension added, refactoring

---
+++ BuildInCommand.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using static To_Do_List.Program;
+
+namespace To_Do_List
+{
+    internal static class BuildInCommand
+    {
+        public static void CreateTask(this TaskManager saved, string newTask)
+        {
+            if (newTask != null)
+            {
+                saved.tasks.Add(new Program.Task()
+                {
+                    Body = newTask,
+                    IsCompleted = false
+                });
+                Console.WriteLine("Task created!");
+            }
+            else Console.WriteLine("Not valid task, try again");
+        }
+
+        public static void ShowTasks(this TaskManager saved)
+        {
+            if (saved.tasks.Count > 0)
+            {
+                for (int i = 0; i < saved.tasks.Count(); i++)
+                {
+                    Console.WriteLine($"{BuildInCommand.ToShowId(i)}: {saved.tasks[i].Body}, Completed: {saved.tasks[i].IsCompleted}");
+                }
+            }
+            else Console.WriteLine("No tasks found");
+        }
+
+        public static void DeleteTask(this TaskManager saved, int Index)
+        {
+            saved.tasks.RemoveAt(BuildInCommand.ToInternalId(Index));
+            Console.WriteLine($"Task# {Index} successfully deleted!");
+        }
+
+        public static void CompleteTask(this TaskManager saved, int Index)
+        {
+            saved.tasks[BuildInCommand.ToInternalId(Index)].IsCompleted = true;
+            Console.WriteLine($"Task# {Index} successfully completed!");
+        }
+
+        public static bool Validation(this TaskManager saved, string cmd, int index)
+        {
+            var internalId = BuildInCommand.ToInternalId(index);
+            Console.WriteLine($"Are you sure you want to {cmd} Task# {index}? yes/no");
+            if (Console.ReadLine() == "yes" && internalId < saved.tasks.Count() && internalId >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                return false;
+            }
+        }
+
+        public static string TaskParse(string input)
+        {
+            if (input.Split(' ', 2).Length > 1)
+            {
+                return input.Split(' ', 2)[1];
+            }
+            else return null;
+        }
+
+        public static int IdParse(string input)
+        {
+            int result;
+            if (int.TryParse(input, out result)) return result;
+            else return 0;
+        }
+
+        public static int ToShowId(int id)
+        {
+            return id + 1;
+        }
+
+        public static int ToInternalId(int id)
+        {
+            return id - 1;
+        }
+
+        public static void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,23 +16,23 @@
                     switch (cmd)
                     {
                         case "add":
-                            BuildInCommand.CreateTask(SavedTasks.Instance, task); 
+                            TaskManager.Instance.CreateTask(task);
                             break;
                         case "show":
-                            BuildInCommand.ShowTasks(SavedTasks.Instance);
+                            TaskManager.Instance.ShowTasks();
                             break;
                         case "delete":
                             var idDel = BuildInCommand.IdParse(task);
-                            if (BuildInCommand.Validation(SavedTasks.Instance, cmd, idDel))
+                            if (TaskManager.Instance.Validation(cmd, idDel))
                             {
-                                BuildInCommand.DeleteTask(SavedTasks.Instance, idDel);
+                                TaskManager.Instance.DeleteTask(idDel);
                             }
                             break;
                         case "complete":
                             var idComp = BuildInCommand.IdParse(task);
-                            if (BuildInCommand.Validation(SavedTasks.Instance, cmd, idComp))
+                            if (TaskManager.Instance.Validation(cmd, idComp))
                             {
-                                BuildInCommand.CompleteTask(SavedTasks.Instance, idComp);
+                                TaskManager.Instance.CompleteTask(idComp);
                             }
                             break;
                         case "exit":
@@ -59,106 +59,22 @@
             public bool IsCompleted { get; set; }
         }
 
-        public class SavedTasks
+        public class TaskManager
         {
-            private SavedTasks() { }
-            private static SavedTasks instance;
-            public static SavedTasks Instance
+            private TaskManager() { }
+            private static TaskManager instance;
+            public static TaskManager Instance
             {
                 get
                 {
                     if (instance == null)
                     {
-                        instance = new SavedTasks();
+                        instance = new TaskManager();
                     }
                     return instance;
                 }
             }
             public List<Task> tasks = new List<Task>();
-        }
-
-        public static class BuildInCommand
-        {
-            public static void CreateTask (SavedTasks saved, string newTask)
-            {
-                if (newTask != null)
-                {
-                    saved.tasks.Add(new Task()
-                    {
-                        Body = newTask,
-                        IsCompleted = false
-                    });
-                    Console.WriteLine("Task created!");
-                }
-                else Console.WriteLine("Not valid task, try again");
-            }
-
-            public static void ShowTasks (SavedTasks saved)
-            {
-                if (saved.tasks.Count > 0)
-                {
-                    for (int i = 0; i < saved.tasks.Count(); i++)
-                    {
-                        Console.WriteLine($"{BuildInCommand.ToShowId(i)}: {saved.tasks[i].Body}, Completed: {saved.tasks[i].IsCompleted}");
-                    }
-                }
-                else Console.WriteLine("No tasks found");
-            }
-
-            public static void DeleteTask(SavedTasks saved, int Index)
-            {
-                saved.tasks.RemoveAt(BuildInCommand.ToInternalId(Index));
-                Console.WriteLine($"Task# {Index} successfully deleted!");
-            }
-
-            public static void CompleteTask(SavedTasks saved, int Index)
-            {
-                saved.tasks[BuildInCommand.ToInternalId(Index)].IsCompleted = true;
-                Console.WriteLine($"Task# {Index} successfully completed!");
-            }
-
-            public static bool Validation (SavedTasks saved, string cmd, int index)
-            {
-                var internalId = BuildInCommand.ToInternalId(index);
-                Console.WriteLine($"Are you sure you want to {cmd} Task# {index}? yes/no");
-                if (Console.ReadLine() == "yes" && internalId < saved.tasks.Count() && internalId >= 0)
-                {
-                    return true;
-                } else {
-                    Console.WriteLine("Try again");
-                    return false;
-                } 
-            } 
-
-            public static string TaskParse (string input) {
-                if (input.Split(' ', 2).Length > 1)
-                {
-                    return input.Split(' ', 2)[1];
-                }
-                else return null;
-            }
-
-            public static int IdParse(string input)
-            {
-                int result;
-                if (int.TryParse(input, out result)) return result;
-                else return 0;
-            }
-
-            public static int ToShowId (int id)
-            {
-                return id + 1;
-            }
-
-            public static int ToInternalId(int id)
-            {
-                return id - 1;
-            }
-
-            public static void Exit()
-            {
-                Environment.Exit(0);
-            }
         }
 
     }
